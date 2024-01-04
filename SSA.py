c1a962f75d729a0f85c105bd5e89f5d3bee745c5
omar.ben-said <omar.ben-said@polytechnique.edu>
2024-01-04 19:21:38 +0100

    added transform and fit_transform

--- SSA.py
+++ SSA.py
@@ -70,7 +70,81 @@ class SSA:
         eigvals, eigvecs = eigvals[:self.r], eigvecs[:, :self.r]
         self.weights = eigvals
         self.U = eigvecs
-        
+    
+    def transform(self, data):
+        """
+            Transform the given data using the SSA model.
+
+            This function takes in the data and transforms it using the SSA model. It first computes the trajectory matrix and then projects it onto the principal components.
+
+            Parameters
+            ----------
+            data : np.ndarray
+                The input data. It should be a 2D array where the first dimension is the number of features and the second dimension is the number of samples.
+
+            Returns
+            -------
+            np.ndarray
+                The transformed data. It is a 2D array where the first dimension is the number of features and the second dimension is the number of samples.
+
+            Notes
+            -----
+            The function does not modify the object.
+
+            Raises
+            ------
+            AssertionError
+                If the input data is not a 2D array or if the number of samples is less than the number of features.
+        """
+        # assuming data is of shape (n_features, n_samples)
+        assert len(data.shape) == 2, "data must be 2D"
+        assert data.shape[1] > data.shape[0], "data must have more samples than features"
+        N = data.shape[1]
+        M = data.shape[0]
+        if self.L == "auto":
+            # do some transformation to get the L value
+            if self.type == "HSSA":
+                self.L = int(M * (N+1)/(M+1))
+            else:
+                self.L = int((N+1)/(M+1))
+        # constructing the trajectory matrix for each dimension
+        X = [self.get_trajectory(data[i], self.L) for i in range(M)]
+        if self.type == "HSSA":
+            # we first horizontally stack the data
+            # then we do the trajectory matrix
+            X = np.hstack(X)
+        else: 
+            # we first vertically stack the data
+            # then we do the trajectory matrix
+            X = np.vstack(X)
+        return self.U @ self.U.T @ X
+    def fit_transform(self, data):
+        """
+            Fit the SSA model to the given data and then transform it.
+
+            This function takes in the data, computes the principal components, and assigns them to `self.U`. It also computes the associated eigenvalues and assigns them to `self.weights`. It then transforms the data using the SSA model.
+
+            Parameters
+            ----------
+            data : np.ndarray
+                The input data. It should be a 2D array where the first dimension is the number of features and the second dimension is the number of samples.
+
+            Returns
+            -------
+            np.ndarray
+                The transformed data. It is a 2D array where the first dimension is the number of features and the second dimension is the number of samples.
+
+            Notes
+            -----
+            The function modifies the object by setting the `self.U` and `self.weights` attributes. `self.U` contains the principal components and `self.weights` contains the associated eigenvalues.
+
+            Raises
+            ------
+            AssertionError
+                If the input data is not a 2D array or if the number of samples is less than the number of features.
+        """
+        self.fit(data)
+        return self.transform(data)
     def get_trajectory(self, data, L):
         # assuming data is of shape (n_samples,)
         assert len(data.shape) == 1, "data must be 1D"
