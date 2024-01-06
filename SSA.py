2ee126b55018ca85fad24acb16fb73b12b57d212
omar.ben-said <omar.ben-said@polytechnique.edu>
2024-01-06 20:27:28 +0100

    Added the predict

--- SSA.py
+++ SSA.py
@@ -151,20 +151,29 @@ class SSA:
         for i in range(N-L+1):
             X[i] = data[i:i+L]
         return X.T
-    def hankelization(self, X):
-        if self.type == "HSSA":
-            X = np.hsplit(X, self.M)
+    def hankelization(self, X, test=True):
+        if test:
+            if self.type == "HSSA":
+                X = np.hsplit(X, self.M)
+            else:
+                X = np.vsplit(X, self.M)
+            temp = np.zeros((self.M, X[0].shape[0] + X[0].shape[1] - 1))
+            for i in range(len(X)):
+                for j in range(X[i].shape[0]):
+                    for k in range(X[i].shape[1]):
+                        temp[i, j+k] += X[i][j, k]
+            for j in range(temp.shape[1]):
+                temp[:,j] /= min(j+1, temp.shape[1]-j)
+            temp = [hankel(temp[i, :self.L], temp[i, self.L-1:]) for i in range(self.M)]
+            return np.hstack(temp) if self.type == "HSSA" else np.vstack(temp)
         else:
-            X = np.vsplit(X, self.M)
-        temp = np.zeros((self.M, X[0].shape[0] + X[0].shape[1] - 1))
-        for i in range(len(X)):
-            for j in range(X[i].shape[0]):
-                for k in range(X[i].shape[1]):
-                    temp[i, j+k] += X[i][j, k]
-        for j in range(temp.shape[1]):
-            temp[:,j] /= min(j+1, temp.shape[1]-j)
-        temp = [hankel(temp[i, :self.L], temp[i, self.L-1:]) for i in range(self.M)]
-        return np.hstack(temp) if self.type == "HSSA" else np.vstack(temp)
+            temp = np.zeros(X.shape[0] + X.shape[1] - 1)
+            for j in range(X.shape[0]):
+                    for k in range(X.shape[1]):
+                        temp[j+k] += X[j, k]
+            for j in range(temp.shape[0]):
+                temp[j] /= min(j+1, temp.shape[0]-j)
+            return hankel(temp[:self.L], temp[self.L-1:])
     def predict(self, h=1, method="recurrent", starting_point = None):
         """
         Predicts future values using the SSA model.
@@ -223,18 +232,54 @@ class SSA:
         elif method == "vector":
             if self.type == "VSSA":
                 indices = list(range(self.L-1, self.U.shape[0], self.L))
-                W = self.U[indices, :]
+                W = self.U[indices]
                 U_M = np.delete(self.U, indices, axis=0)
                 R = U_M @ W.T @ np.linalg.pinv((np.eye(self.M) - W @ W.T))
-                pi = U_M @ U_M.T + R @ (np.eye(self.M) - W @ W.T) @ R.T
-                pass
+                PI = U_M @ U_M.T + R @ (np.eye(self.M) - W @ W.T) @ R.T
+                PI = np.vsplit(PI, self.M)
+                print(f"PI: {len(PI)}")
+                print(f"R: {len(R)}")
+                Y_delta = self.X_tilda[:, -1]
+                Y_delta = np.delete(Y_delta, list(range(self.L-1, Y_delta.shape[0], self.L)), axis=0)
+                temp = []
+                for i in range(self.M):
+                    temp.append(PI[i])
+                    temp.append(R[:,i].T)
+                projector = np.vstack(temp)
+                del temp
+                result = np.empty((h, self.M))
+                Z_i = self.X_tilda[:, min(-self.L + h, -1):]
+                indices = list(range(self.L-1, Z_i.shape[0], self.L))
+                mask = np.ones_like(Z_i[:, -1], np.bool_)
+                mask[indices] = False
+                for i in range(h):
+                    temp = projector @ Z_i[:,-1][mask]
+                    Z_i = np.concatenate((Z_i, temp[:, None]), axis = -1)
+                Z_i = self.hankelization(Z_i)
+                result = Z_i[np.array(list(range(self.L-1, Z_i.shape[0], self.L))), -h:]
+                return result
+            
+
             elif self.type == "HSSA":
-                U_H = self.U[self.L-1, :]
-                pi_H = self.U[-1, :]
+                U_H = self.U[:self.L-1]
+                pi_H = self.U[-1]
                 v_squared = np.dot(pi_H, pi_H)
                 R = (1/(1 - v_squared)) * U_H @ pi_H
-                pi = U_H @ U_H.T  + (1 - v_squared) * R @ R.T
-                pass
+                PI = U_H @ U_H.T  + (1 - v_squared) * R @ R.T
+                last_columns = list(range(self.X_tilda.shape[1]//self.M - 1, self.X_tilda.shape[1], self.X_tilda.shape[1]//self.M))
+                Y_delta = self.X_tilda[1:, last_columns]
+                projector = np.vstack((PI, R.T))
+                result = np.empty((h, self.M))
+                for i in range(self.M):
+                    Z_i = self.X_tilda[:, (0 if i == 0 else last_columns[i-1]):last_columns[i]]
+                    for j in range(h):
+                        temp = projector @ Z_i[1:, -1]
+                        Z_i = np.hstack((Z_i, temp[:, None]))
+                    Z_i = self.hankelization(Z_i, test=False)
+                    result[:, i] = Z_i[-1, -h:]
+                return result
+
+                
 
     def score(data):
         pass
